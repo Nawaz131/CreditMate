5f54854aa2726553750374ad5445d18f41bb40f4
Nawaz Hussain <nawazbangalore@example.com>
2026-09-16 00:19:15 +0530

    Update backend

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://creditmate-frontend.onrender.com",
         methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
         credentials: true,
     })
